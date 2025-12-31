dad5e339a46d7e99b2ba0184717da0936dfb05bd
Le Quang Tuyen <124868890+lequangtuyen543@users.noreply.github.com>
2025-12-31 17:52:32 +0700

    6.2 + 6.3

---
+++ api/v1/controllers/task.controller.js
@@ -0,0 +1,26 @@
+const Task = require("../models/task.model");
+
+// [GET] /api/v1/tasks
+module.exports.index = async (req, res) => {
+  const tasks = await Task.find({
+    deleted: false,
+  });
+
+  res.json(tasks);
+};
+
+// [GET] /api/v1/tasks/detail/:id
+module.exports.detail = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const tasks = await Task.findOne({
+      _id: id,
+      deleted: false,
+    });
+
+    res.json(tasks);
+  } catch (error) {
+    res.json("không tìm thấy!");
+  }
+};
--- api/v1/routes/task.route.js
+++ api/v1/routes/task.route.js
@@ -1,29 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const Task = require("../../../models/task.model");
+const controller = require("../../v1/controllers/task.controller");
 
-router.get("/", async (req, res) => {
-  const tasks = await Task.find({
-    deleted: false,
-  });
+router.get("/", controller.index);
 
-  res.json(tasks);
-});
-
-router.get("/detail/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-
-    const tasks = await Task.findOne({
-      _id: id,
-      deleted: false,
-    });
-
-    res.json(tasks);
-  } catch (error) {
-    res.json("không tìm thấy!");
-  }
-});
+router.get("/detail/:id", controller.detail);
 
 module.exports = router;
