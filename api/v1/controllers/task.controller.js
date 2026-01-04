38448fe29f9eeaff6c41b7e917da79b4cd410310
Le Quang Tuyen <124868890+lequangtuyen543@users.noreply.github.com>
2026-01-04 20:50:53 +0700

    4. Đổi trạng thái nhiều công việc

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -86,3 +86,41 @@ module.exports.changeStatus = async (req, res) => {
     });
   }
 };
+
+// [PATCH] /api/v1/tasks/change-multi
+module.exports.changeMulti = async (req, res) => {
+  try {
+    const { ids, key, value } = req.body;
+
+    console.log(ids, key, value)
+
+    switch (key) {
+      case "status":
+        await Task.updateMany(
+          {
+            _id: { $in: ids },
+          },
+          {
+            status: value,
+          }
+        );
+        res.json({
+          code: 200,
+          message: "Cập nhật trạng thái thành công!",
+        });
+        break;
+
+      default:
+        res.json({
+          code: 400,
+          message: "Không tồn tại !",
+        });
+        break;
+    }
+  } catch (error) {
+    res.json({
+      code: 400,
+      message: "Không tồn tại!",
+    });
+  }
+};
